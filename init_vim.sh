fe285fc87d3d85a2594c578a68da029089c7c608
邵文良 <5238247+wilight@user.noreply.gitee.com>
2019-12-23 11:35:37 +0800

    update vim config

--- init_vim.sh
+++ init_vim.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 echoo ">>> Install vim ..."
-./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-pythoninterp \
-            #--with-python-config-dir=/usr/lib64/python2.7/config \
-            --enable-python3interp \
-            #--with-python3-config-dir=/usr/lib/python3.4/config \
-            --with-mac-arch=x86_64 \
-            --enable-gui=gtk2 --enable-cscope --prefix=/usr
-make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
-sudo make install
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge --enable-multibyte --enable-pythoninterp \
+    --with-python-config-dir=/usr/lib64/python2.7/config \
+    --with-python-command=/usr/bin/python \
+    --enable-gui=gtk2 --enable-cscope --prefix=~/data/vim8
+make -j4
+make install 
--- spf13-vim.sh
+++ spf13-vim.sh
@@ -16,7 +16,7 @@
 
 ############################  SETUP PARAMETERS
 app_name='spf13-vim'
-[ -z "$APP_PATH" ] && APP_PATH="/data1/shaowenliang/envs/spf13_data"
+[ -z "$APP_PATH" ] && APP_PATH="~/data/envs/spf13_data"
 [ -z "$REPO_URI" ] && REPO_URI='https://github.com/spf13/spf13-vim.git'
 [ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
 debug_mode='0'
