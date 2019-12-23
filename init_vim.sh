c925cbb5109977ea3335b6811f8b02fa42aafc04
邵文良 <5238247+wilight@user.noreply.gitee.com>
2019-12-23 11:55:36 +0800

    update vim config

--- init_vim.sh
+++ init_vim.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-echoo ">>> Install vim ..."
+echo ">>> Install vim ..."
 git clone https://github.com/vim/vim.git
 cd vim
 ./configure --with-features=huge --enable-multibyte --enable-pythoninterp \
     --with-python-config-dir=/usr/lib64/python2.7/config \
     --with-python-command=/usr/bin/python \
-    --enable-gui=gtk2 --enable-cscope --prefix=~/data/vim8
-make -j4 VIMRUNTIMEDIR=~/data/vim8/vim_runtime
+    --enable-gui=gtk2 --enable-cscope --prefix=/home/shaowenliang/data/vim8
+make -j4 VIMRUNTIMEDIR=/home/shaowenliang/data/vim8/vim_runtime
 make install 
 echo "PATH=/home/shaowenliang/data/vim8/bin:\$PATH" >> ~/.bashrc
