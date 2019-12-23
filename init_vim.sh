57d3b1a81716aa97bf03b49ab65e1e3f34709a30
邵文良 <5238247+wilight@user.noreply.gitee.com>
2019-12-23 11:49:52 +0800

    updata vim config

--- init_vim.sh
+++ init_vim.sh
@@ -6,5 +6,6 @@ cd vim
     --with-python-config-dir=/usr/lib64/python2.7/config \
     --with-python-command=/usr/bin/python \
     --enable-gui=gtk2 --enable-cscope --prefix=~/data/vim8
-make -j4
+make -j4 VIMRUNTIMEDIR=~/data/vim8/vim_runtime
 make install 
+echo "PATH=/home/shaowenliang/data/vim8/bin:\$PATH" >> ~/.bashrc
